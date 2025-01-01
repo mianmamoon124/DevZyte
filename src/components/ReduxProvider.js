efc9c34681a30cda01b171f1d417b049048c6be8
mianmamoon124 <150371603+mianmamoon124@users.noreply.github.com>
2025-01-01 23:08:38 +0500

    ReduxProvider.js

--- src/components/ReduxProvider.js
+++ src/components/ReduxProvider.js
@@ -1,12 +1,1 @@
-// components/ReduxProvider.js
 
-'use client'; // Ensure this is a client-side component
-
-import { Provider } from 'react-redux';
-import store from '../store/store'; // Path to your Redux store
-
-const ReduxProvider = ({ children }) => {
-  return <Provider store={store}>{children}</Provider>;
-};
-
-export default ReduxProvider;
